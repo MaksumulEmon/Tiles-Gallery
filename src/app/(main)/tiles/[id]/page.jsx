d8f6bf2184b1df53cfbabe13c7b787195980e292
MaksumulEmon <maksumulemon300@gmail.com>
2026-04-30 22:48:10 +0600

    Detiles page Add

--- src/app/(main)/tiles/[id]/page.jsx
+++ src/app/(main)/tiles/[id]/page.jsx
@@ -1,13 +1,75 @@
+import { Button, Card, CloseButton } from '@heroui/react';
+import Image from 'next/image';
 import React from 'react';
 
-const DetailsPage = async ({params}) => {
-    const {id} =  await params;
-    console.log(id)
+const DetailsPage = async ({ params }) => {
+    const { id } = await params;
+    // console.log(id)
+    const res = await fetch("https://tiles-gallery-three.vercel.app/data.json")
+    const tiles = await res.json();
+
+
+    const tile = tiles.find(t => t.id == id)
+
+    // console.log(tile);
     return (
-        <div>
-            Tiles Details....
+        <div className="max-w-7xl mx-auto px-4 md:px-0 pt-4">
+
+            <div className="bg-white shadow-xl rounded-2xl overflow-hidden ">
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
+
+
+                    <div className="relative w-full aspect-square">
+                        <Image
+                            src={tile.image}
+                            alt={tile.title}
+                            fill
+                            sizes="(max-width: 768px) 100vw,(max-width:1200px) 50vw, 33vw"
+                            className=" rounded object-cover "
+                        />
+                    </div>
+
+
+
+                    <div className="p-6 space-y-4 mx-20">
+
+                        <h1 className="text-2xl md:text-4xl font-bold text-gray-800 text-center">
+                            {tile.title}
+                        </h1>
+
+                        <p className="text-gray-500 text-2xl font-semibold">
+                            Category: <span className="font-medium  text-gray-700">{tile.category}</span>
+                        </p>
+
+                        <p className="text-gray-600 leading-relaxed">
+                            {tile.description}
+                        </p>
+
+                        <div className="grid grid-cols-2 gap-4 pt-4">
+
+                            <div className="bg-gray-100 p-3 rounded-lg flex gap-2">
+                                <p className="text-sm text-gray-500">Price</p>
+                                <p className="font-semibold text-gray-800">{tile.price} $</p>
+                            </div>
+
+                            <div className='flex justify-between items-center'>
+                                <p>{tile.dimensions}</p>
+                                <p className={`px-3 py-1 rounded-xl text-white font-semibold ${tile.inStock ? "bg-[#79AE6F] " : "bg-[#AE2448]"}`}>   {tile.inStock ? "Available Stock" : "Out of Stock"}</p>
+                            </div>
+
+                        </div>
+
+                        <button className="mt-4 w-full bg-green-600 hover:bg-green-700 text-white py-3 rounded-xl transition">
+                            Buy Now
+                        </button>
+
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
 
-export default DetailsPage ;
+export default DetailsPage;
--- src/components/homepage/TitleMarque.jsx
+++ src/components/homepage/TitleMarque.jsx
@@ -10,23 +10,25 @@ const news = [
 
 const TitleMarque = () => {
     return (
-        <div className="group flex items-center border-b border-neutral-200 bg-white py-2">
-            <button className="flex items-center gap-3 px-8 text-[11px] font-medium tracking-[0.3em] uppercase text-neutral-900 transition-all ">
-                <span className=" w-4 bg-neutral-900"></span>
-                Update Collection
-            </button>
+        <div className="max-w-7xl mx-auto">
+            <div className="group flex items-center border-b border-neutral-200 bg-white py-2">
+                <button className="flex items-center gap-3 px-8 text-[11px] font-medium tracking-[0.3em] uppercase text-neutral-900 transition-all ">
+                    <span className=" w-4 bg-neutral-900"></span>
+                    Update Collection
+                </button>
 
-          
-            <Marquee speed={35} pauseOnHover={true} className="overflow-hidden lg:mr-30">
-                {news.map((item) => (
-                    <div key={item.id} className="flex items-center">
-                        <span className="mx-12  text-[12px] font-light italic tracking-widest text-black">
-                            {item.title}
-                        </span>
-                        <span className="text-black">/</span>
-                    </div>
-                ))}
-            </Marquee>
+
+                <Marquee speed={35} pauseOnHover={true} className="overflow-hidden lg:mr-30">
+                    {news.map((item) => (
+                        <div key={item.id} className="flex items-center">
+                            <span className="mx-12  text-[12px] font-light italic tracking-widest text-black">
+                                {item.title}
+                            </span>
+                            <span className="text-black">/</span>
+                        </div>
+                    ))}
+                </Marquee>
+            </div>
         </div>
     );
 };
