e163a707dac7958e93a80d7c3fd53e4fb482cd75
MaksumulEmon <maksumulemon300@gmail.com>
2026-04-30 23:34:12 +0600

    Spinner loading

---
+++ src/app/(main)/alltiles/loading.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FadeLoader } from 'react-spinners';
+
+const Loading = () => {
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh'
+        }}>
+            <FadeLoader />
+        </div>
+    );
+};
+
+export default Loading;
